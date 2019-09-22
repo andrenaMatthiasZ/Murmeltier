a9ad72016f76bdf0305b8af212e00b45aed8fcf0
Matthias Zeleny <matthias.zeleny@andrena.de>
2019-09-22 12:11:51 +0200

    add main list

--- app/src/main/java/com/example/murmeltier/MainActivity.kt
+++ app/src/main/java/com/example/murmeltier/MainActivity.kt
@@ -5,15 +5,30 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var listView: ListView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        listView = findViewById(R.id.list)
+
+        var arrayList:ArrayList<String> = ArrayList()
+
+        arrayList.add("First")
+        arrayList.add("Second")
+
+        var arrayAdapter: ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList)
+
+        listView.adapter = arrayAdapter
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Test", Snackbar.LENGTH_LONG)
@@ -36,4 +51,6 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+
 }
