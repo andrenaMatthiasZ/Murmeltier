89254b50ac017e94420f34c0f9518db71e1f4b76
EineZeileGehtNoch <einezeilegehtnoch@gmail.com>
2019-11-05 10:46:05 +0100

    fix formatting

--- app/src/main/java/com/example/murmeltier/ToDoListAdapter.kt
+++ app/src/main/java/com/example/murmeltier/ToDoListAdapter.kt
@@ -7,7 +7,7 @@ import android.widget.ArrayAdapter
 import android.widget.TextView
 
 class ToDoListAdapter(context: Activity, private val toDos: ArrayList<ToDo>) :
-    ArrayAdapter<ToDo>(context,R.layout.todo_list, toDos) {
+    ArrayAdapter<ToDo>(context, R.layout.todo_list, toDos) {
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val view = TextView(context)
 
